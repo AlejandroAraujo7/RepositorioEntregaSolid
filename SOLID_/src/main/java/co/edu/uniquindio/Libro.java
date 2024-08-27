d035038dc42716efa5eb3ab6a91177f4710b1b40
Alejandro <jheniera.araujom@uqvirtual.edu.co>
2024-08-27 12:26:10 -0500

    se agrega la clase libro

--- SOLID_/src/main/java/co/edu/uniquindio/Libro.java
+++ SOLID_/src/main/java/co/edu/uniquindio/Libro.java
@@ -1,4 +1,44 @@
 package co.edu.uniquindio;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponible;
+
+    public Libro(String titulo, String autor, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponible = true; // por defecto, el libro está disponible
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void prestar() {
+        this.disponible = false;
+    }
+
+    public void devolver() {
+        this.disponible = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Título: " + titulo + ", Autor: " + autor + ", ISBN: " + isbn + ", Disponible: " + disponible;
+    }
 }
