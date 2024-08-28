79afad772fa5ea29f7a097bf7a0fad5ad6e7c698
montielluza20 <160981571+montielluza20@users.noreply.github.com>
2024-08-27 22:37:07 -0500

    Se crea la clase Bibliotecario

---
+++ SOLID_/src/main/java/co/edu/uniquindio/Bibliotecario.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio;
+
+public class Bibliotecario {
+    public Bibliotecario(String nombre, String idEmpleado) {
+        super(nombre, idEmpleado);
+    }
+
+    public void gestionarPrestamos(Prestamo prestamo) {
+        // Implementar lógica para gestionar préstamos
+    }
+
+}
